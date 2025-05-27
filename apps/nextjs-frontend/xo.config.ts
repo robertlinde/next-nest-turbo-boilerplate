3428c4c18845b82c596fa0d266fbf90be4bd8123
Robert Linde <linde.robert@t-online.de>
2025-05-27 23:34:30 +0200

    chore: remove unused lint plugins

--- apps/nextjs-frontend/xo.config.ts
+++ apps/nextjs-frontend/xo.config.ts
@@ -1,27 +1,10 @@
 import {type FlatXoConfig} from 'xo';
-// eslint-disable-next-line n/no-extraneous-import
-import tseslint from 'typescript-eslint';
-import {type ESLint, type Linter} from 'eslint';
-import unicorn from 'eslint-plugin-unicorn';
 
 const xoConfig: FlatXoConfig = [
-  // Extend the base config by merging its properties with nestjs specific additions
   {
     ignores: ['node_modules', 'postcss.config.mjs', 'commitlint.config.js'],
   },
   {
-    plugins: {
-      '@typescript-eslint': tseslint.plugin as ESLint.Plugin,
-      unicorn,
-    },
-    languageOptions: {
-      parser: tseslint.parser as Linter.Parser,
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-        sourceType: 'module',
-      },
-    },
     react: true,
     prettier: 'compat',
     space: true,
