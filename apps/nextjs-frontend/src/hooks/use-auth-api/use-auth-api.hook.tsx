70d434a1a1133eee38200424b470b27c20f5d87c
Robert Linde <linde.robert@t-online.de>
2025-05-28 12:46:01 +0200

    fix(For useAuthApi, decuple types used in services from forms #101): fix imports

--- apps/nextjs-frontend/src/hooks/use-auth-api/use-auth-api.hook.tsx
+++ apps/nextjs-frontend/src/hooks/use-auth-api/use-auth-api.hook.tsx
@@ -1,19 +1,19 @@
 'use client';
 
-import {type ConfirmHandlerOptions} from './handlers/confirm/types/confirm-handler-options.type.ts';
-import {type LoginTwoFactorHandlerOptions} from './handlers/use-login-two-factor/types/login-two-factor-handler-options.type.ts';
-import {type LogoutHandlerOptions} from './handlers/use-logout/types/logout-handler-options.type.ts';
-import {type RegisterHandlerOptions} from './handlers/use-register/types/register-handler-options.type.ts';
-import {type ResetPasswordHandlerOptions} from './handlers/use-reset-password/types/reset-password-handler-options.type.ts';
-import {useConfirm} from './handlers/confirm/use-confirm.hook.tsx';
-import {useForgotPassword} from './handlers/use-forgot-password/use-forgot-password.hook.tsx';
-import {useResetPassword} from './handlers/use-reset-password/use-reset-password.hook.tsx';
-import {useLoginCredentials} from './handlers/use-login-credentials/use-login-credentials.hook.tsx';
-import {type LoginCredentialsHandlerOptions} from './handlers/use-login-credentials/types/login-credentials-handler-options.type.ts';
-import {type ForgotPasswordHandlerOptions} from './handlers/use-forgot-password/types/forgot-password-handler-options.type.ts';
-import {useLoginTwoFactor} from './handlers/use-login-two-factor/use-login-two-factor.hook.tsx';
-import {useLogout} from './handlers/use-logout/use-logout.hook.tsx';
-import {useRegister} from './handlers/use-register/use-register.hook.tsx';
+import {type ConfirmHandlerOptions} from './hooks/use-confirm/types/confirm-handler-options.type.ts';
+import {useConfirm} from './hooks/use-confirm/use-confirm.hook.tsx';
+import {type ForgotPasswordHandlerOptions} from './hooks/use-forgot-password/types/forgot-password-handler-options.type.ts';
+import {useForgotPassword} from './hooks/use-forgot-password/use-forgot-password.hook.tsx';
+import {type LoginCredentialsHandlerOptions} from './hooks/use-login-credentials/types/login-credentials-handler-options.type.ts';
+import {useLoginCredentials} from './hooks/use-login-credentials/use-login-credentials.hook.tsx';
+import {type LoginTwoFactorHandlerOptions} from './hooks/use-login-two-factor/types/login-two-factor-handler-options.type.ts';
+import {useLoginTwoFactor} from './hooks/use-login-two-factor/use-login-two-factor.hook.tsx';
+import {type LogoutHandlerOptions} from './hooks/use-logout/types/logout-handler-options.type.ts';
+import {useLogout} from './hooks/use-logout/use-logout.hook.tsx';
+import {type RegisterHandlerOptions} from './hooks/use-register/types/register-handler-options.type.ts';
+import {useRegister} from './hooks/use-register/use-register.hook.tsx';
+import {type ResetPasswordHandlerOptions} from './hooks/use-reset-password/types/reset-password-handler-options.type.ts';
+import {useResetPassword} from './hooks/use-reset-password/use-reset-password.hook.tsx';
 
 /**
  * A custom hook that provides methods for various authentication-related actions such as login,
