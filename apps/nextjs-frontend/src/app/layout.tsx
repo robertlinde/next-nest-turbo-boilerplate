7cceab616155d894303b7328205be561903f4384
Robert Linde <linde.robert@t-online.de>
2025-06-16 22:34:03 +0200

    feat(Add footer, imprint and data protection page #181): add basic footer component

--- apps/nextjs-frontend/src/app/layout.tsx
+++ apps/nextjs-frontend/src/app/layout.tsx
@@ -10,6 +10,7 @@ import {ReactQueryProvider} from '@/providers/react-query/react-query.provider';
 import {ToastProvider} from '@/providers/toast/toast.provider';
 import {UserProvider} from '@/providers/user/user.provider';
 import {Header} from '@/components/header/header.component.tsx';
+import {Footer} from '@/components/footer/footer.component';
 
 export const metadata: Metadata = {
   title: 'Next.js Frontend',
@@ -29,9 +30,10 @@ export default function RootLayout({
           <UserProvider>
             <ReactQueryProvider>
               <Header />
-              <div className="mx-auto my-6 flex w-full max-w-7xl flex-col px-2 md:my-8 md:px-4 lg:my-12">
+              <div className="mx-auto my-6 flex w-full max-w-7xl flex-col px-2 md:my-8 md:px-4 lg:my-12 min-h-screen">
                 {children}
               </div>
+              <Footer />
             </ReactQueryProvider>
           </UserProvider>
         </ToastProvider>
