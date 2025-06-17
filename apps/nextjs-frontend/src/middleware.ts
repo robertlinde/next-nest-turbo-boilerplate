860d660d2b693bdb314ae0936ee64071e44b2023
Robert Linde <linde.robert@t-online.de>
2025-06-17 09:13:14 +0200

    fix(Set up internationalization #182): fix middleware matcher

--- apps/nextjs-frontend/src/middleware.ts
+++ apps/nextjs-frontend/src/middleware.ts
@@ -5,8 +5,11 @@ import {routing} from './i18n/routing.ts';
 export default createMiddleware(routing);
 
 export const config: MiddlewareConfig = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: String.raw`/((?!api|trpc|_next|_vercel|.*\..*).*)`,
+  matcher: [
+    // Match all pathnames except for
+    // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
+    // - … the ones containing a dot (e.g. `favicon.ico`)
+    // eslint-disable-next-line unicorn/prefer-string-raw
+    '/((?!api|trpc|_next|_vercel)(?!.*\\..*).*)',
+  ],
 };
