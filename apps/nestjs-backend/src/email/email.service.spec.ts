ffe2f89d1816ba4e846f15d7a3b8fe7e9c4feca7
Robert Linde <linde.robert@t-online.de>
2025-06-19 09:52:22 +0200

    fix(fix BE unit tests #196): tests for internationalized emails

--- apps/nestjs-backend/src/email/email.service.spec.ts
+++ apps/nestjs-backend/src/email/email.service.spec.ts
@@ -2,7 +2,6 @@ import {ConfigService} from '@nestjs/config';
 import {Test, TestingModule} from '@nestjs/testing';
 import {MailerService} from '@nestjs-modules/mailer';
 import {mock, MockProxy} from 'jest-mock-extended';
-import {ConfigKey} from '../config/config-key.enum';
 import {EmailService} from './email.service';
 import {AcceptedLanguages} from './types/accepted-languages.enum';
 
@@ -27,12 +26,7 @@ describe('EmailService', () => {
   });
 
   describe('sendConfirmEmail', () => {
-    it('should call mailerService.sendMail when in production environment', async (): Promise<void> => {
-      configService.get.mockImplementation((key: ConfigKey) => {
-        if (key === ConfigKey.NODE_ENV) return 'production';
-        return null;
-      });
-
+    it('should call mailerService.sendMail with English subject and template', async (): Promise<void> => {
       const email = 'test@example.com';
       const confirmationLink = 'http://example.com/confirm';
       const username = 'testuser';
@@ -46,15 +40,25 @@ describe('EmailService', () => {
         context: {username, confirmationLink},
       });
     });
+
+    it('should call mailerService.sendMail with German subject and template', async (): Promise<void> => {
+      const email = 'test@example.com';
+      const confirmationLink = 'http://example.com/confirm';
+      const username = 'testuser';
+
+      await service.sendConfirmEmail(AcceptedLanguages.DE, username, email, confirmationLink);
+
+      expect(mailerService.sendMail).toHaveBeenCalledWith({
+        to: email,
+        subject: 'Bitte bestätigen Sie Ihre E-Mail-Adresse',
+        template: `confirm-user_${AcceptedLanguages.DE}`,
+        context: {username, confirmationLink},
+      });
+    });
   });
 
   describe('sendRequestPasswordResetEmail', () => {
-    it('should call mailerService.sendMail when in production environment', async (): Promise<void> => {
-      configService.get.mockImplementation((key: ConfigKey) => {
-        if (key === ConfigKey.NODE_ENV) return 'production';
-        return null;
-      });
-
+    it('should call mailerService.sendMail with English subject and template', async (): Promise<void> => {
       const email = 'test@example.com';
       const username = 'testuser';
       const passwordResetLink = 'http://example.com/reset';
@@ -71,15 +75,28 @@ describe('EmailService', () => {
         },
       });
     });
+
+    it('should call mailerService.sendMail with German subject and template', async (): Promise<void> => {
+      const email = 'test@example.com';
+      const username = 'testuser';
+      const passwordResetLink = 'http://example.com/reset';
+
+      await service.sendRequestPasswordResetEmail(AcceptedLanguages.DE, email, username, passwordResetLink);
+
+      expect(mailerService.sendMail).toHaveBeenCalledWith({
+        to: email,
+        subject: 'Passwort zurücksetzen',
+        template: `request-password-reset_${AcceptedLanguages.DE}`,
+        context: {
+          username,
+          passwordResetLink,
+        },
+      });
+    });
   });
 
   describe('sendTwoFactorAuthCodeEmail', () => {
-    it('should call mailerService.sendMail when in production environment', async (): Promise<void> => {
-      configService.get.mockImplementation((key: ConfigKey) => {
-        if (key === ConfigKey.NODE_ENV) return 'production';
-        return null;
-      });
-
+    it('should call mailerService.sendMail with English subject and template', async (): Promise<void> => {
       const email = 'test@example.com';
       const code = '123456';
 
@@ -89,6 +106,20 @@ describe('EmailService', () => {
         to: email,
         subject: 'Your 2FA Code',
         template: `two-factor-auth-code_${AcceptedLanguages.EN}`,
+        context: {code},
+      });
+    });
+
+    it('should call mailerService.sendMail with German subject and template', async (): Promise<void> => {
+      const email = 'test@example.com';
+      const code = '123456';
+
+      await service.sendTwoFactorAuthCodeEmail(AcceptedLanguages.DE, email, code);
+
+      expect(mailerService.sendMail).toHaveBeenCalledWith({
+        to: email,
+        subject: 'Ihr 2FA Code',
+        template: `two-factor-auth-code_${AcceptedLanguages.DE}`,
         context: {code},
       });
     });
