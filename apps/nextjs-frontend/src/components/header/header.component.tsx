6ab14a6ace5fea427457b10cb013a9c9d1189b4b
robertlinde <linde.robert@t-online.de>
2025-10-17 15:44:57 +0200

    fix: remove unused var

--- apps/nextjs-frontend/src/components/header/header.component.tsx
+++ apps/nextjs-frontend/src/components/header/header.component.tsx
@@ -3,12 +3,8 @@
 import {useMemo, type JSX} from 'react';
 import {Menubar} from 'primereact/menubar';
 import {type MenuItem} from 'primereact/menuitem';
-import {useTranslations} from 'next-intl';
 
 export function Header(): JSX.Element {
-  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-  const t = useTranslations('Component-Header');
-
   const items: MenuItem[] = useMemo(() => [], []);
 
   return (
